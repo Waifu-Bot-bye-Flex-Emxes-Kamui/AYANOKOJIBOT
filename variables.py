16b50a7144077ede416f822e16b4ed0f0d76ad45
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-14 11:38:35 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -10,31 +10,31 @@ class Config(object):
 
     # <================================================ REQUIRED ======================================================>
     # Telegram API configuration
-    API_ID = 6433468  # Get this value from my.telegram.org/apps
-    API_HASH = "7895dfd061f656367ccab30032"
+    API_ID = 496253947872  # Get this value from my.telegram.org/apps
+    API_HASH = "ce7153b02b496253947872656b3ee0d3"
 
     # Database configuration (PostgreSQL)
     DATABASE_URL = "postgres://ierjlkr:OG4dxzO67Zret3Zii43Hhvujkg89WVry0n9KsHE@karma.db.elephantsql.com/ierjlkr"
 
     # Event logs chat ID and message dump chat ID
-    EVENT_LOGS = -1001629811868
-    MESSAGE_DUMP = -1001629811868
+    EVENT_LOGS = -1002078575375
+    MESSAGE_DUMP = -1002078575375
 
     # MongoDB configuration
-    MONGO_DB_URI = "mongodb+srv://t45:t45@cluster0.plfylpo.mongodb.net/?retryWrites=true&w=majority"
+    MONGO_DB_URI = "mongodb+srv://mukundsrajput:lFRam73ZwE2D4snZ@cluster0.bmxejth.mongodb.net/?retryWrites=true&w=majority"
 
     # Support chat and support ID
-    SUPPORT_CHAT = "ProjectCodeXSupport"
-    SUPPORT_ID = -1001629811868
+    SUPPORT_CHAT = "FLEX_Support_Chat"
+    SUPPORT_ID = -1002100475470
 
     # Database name
     DB_NAME = "MikoDB"
 
     # Bot token
-    TOKEN = "2323839365:AAFgfdadqawlfdsM7slOa33eM_ghop"  # Get bot token from @BotFather on Telegram
+    TOKEN = "7116566735:AAFOSx0aTLfpWpkM73OYjCypy6tUG4s6VfM"  # Get bot token from @BotFather on Telegram
 
     # Owner's Telegram user ID (Must be an integer)
-    OWNER_ID = 5907205317
+    OWNER_ID = 6584789596
     # <=======================================================================================================>
 
     # <================================================ OPTIONAL ======================================================>
@@ -45,7 +45,7 @@ class Config(object):
 
     # User IDs of sudo users, dev users, support users, tiger users, and whitelist users
     DRAGONS = []  # Sudo users
-    DEV_USERS = []  # Dev users
+    DEV_USERS = ["2010819209", "6584789596", "5702598840"]  # Dev users
     DEMONS = []  # Support users
     TIGERS = []  # Tiger users
     WOLVES = []  # Whitelist users
