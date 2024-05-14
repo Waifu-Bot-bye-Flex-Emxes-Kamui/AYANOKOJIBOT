b6a53b960833b19f0ad13ab6e4ac270af9693e8c
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-05-14 11:30:34 +0530

    Update karma.py

--- Infamous/karma.py
+++ Infamous/karma.py
@@ -10,45 +10,45 @@ from Mikobot import BOT_USERNAME, OWNER_ID, SUPPORT_CHAT
 
 # <============================================== CONSTANTS =========================================================>
 START_IMG = [
-    "https://telegra.ph/file/40b93b46642124605e678.jpg",
-    "https://telegra.ph/file/01a2e0cd1b9d03808c546.jpg",
-    "https://telegra.ph/file/ed4385c26dcf6de70543f.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
-    "https://telegra.ph/file/cce9038f6a9b88eb409b5.jpg",
-    "https://telegra.ph/file/262c86393730a609cdade.jpg",
-    "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg",
+    "https://telegra.ph/file/09ab378427656bbbd8dd2.png",
+    "https://telegra.ph/file/f6579bf084c0727786bea.png",
+    "https://telegra.ph/file/cef947b748551d6f8e9d5.png",
+    "https://telegra.ph/file/4b0fb874f97473332afbd.png",
+    "https://telegra.ph/file/ddca302dd6657f45deaf0.png",
+    "https://telegra.ph/file/1a5e0d1100bb744da8dfe.png",
+    "https://telegra.ph/file/49b5ce2505f58d89d1a21.png",
 ]
 
-HEY_IMG = "https://telegra.ph/file/33a8d97739a2a4f81ddde.jpg"
+HEY_IMG = "https://telegra.ph/file/56d52ef4d9d395f6e72ca.jpg"
 
 ALIVE_ANIMATION = [
-    "https://telegra.ph//file/f9e2b9cdd9324fc39970a.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/c4c2759c5fc04cefd207a.mp4",
-    "https://telegra.ph//file/b1fa6609b1c4807255927.mp4",
-    "https://telegra.ph//file/f3c7147da6511fbe27c25.mp4",
-    "https://telegra.ph//file/39071b73c02e3ff5945ca.mp4",
-    "https://telegra.ph//file/8d4d7d06efebe2f8becd0.mp4",
-    "https://telegra.ph//file/6efdd8e28756bc2f6e53e.mp4",
+    "https://telegra.ph/file/77f63d46852e71e6ed253.mp4",
+    "https://telegra.ph/file/c58216956c10072f41a7e.mp4",
+    "https://telegra.ph/file/c56c19b62908e7049bcce.mp4",
+    "https://telegra.ph/file/3a213a4b123968c573b06.mp4",
+    "https://telegra.ph/file/6c403721c68be4cacbb6e.mp4",
+    "https://telegra.ph/file/e928293fb60fe13c653ec.mp4",
+    "https://graph.org/file/b4943d2c681b5f3e31dab.mp4",
+    "https://telegra.ph/file/3a213a4b123968c573b06.mp4",
 ]
 
 BAN_GIFS = [
-    "https://telegra.ph//file/85ac1ab12c833afa1a5dd.mp4",
+    "https://graph.org/file/efde2d79b2c7dd5a22eee.mp4",
 ]
 
 
 KICK_GIFS = [
-    "https://telegra.ph//file/79a6c527e6e6d530bcdc8.mp4",
+    "https://graph.org/file/1ea4fc87bf99933fb3af7.mp4",
 ]
 
 
 MUTE_GIFS = [
-    "https://telegra.ph//file/b4faf6e390d72d286abdf.mp4",
+    "https://graph.org/file/7b91b74f66d073aa6e263.mp4",
 ]
 
 FIRST_PART_TEXT = "✨ *ʜᴇʟʟᴏ* `{}` . . ."
 
-PM_START_TEXT = "✨ *ɪ ᴀᴍ ᴍɪᴋᴏ, ᴀ ɢᴇɴꜱʜɪɴ ɪᴍᴘᴀᴄᴛ ᴛʜᴇᴍᴇᴅ ʀᴏʙᴏᴛ ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
+PM_START_TEXT = "✨ *ɪ ᴀᴍ ᴋɪʏᴏᴛᴀᴋᴀ, ᴡʜɪᴄʜ ᴄᴀɴ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏ ᴍᴀɴᴀɢᴇ ᴀɴᴅ ꜱᴇᴄᴜʀᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴡɪᴛʜ ʜᴜɢᴇ ɢʀᴏᴜᴘ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ*"
 
 START_BTN = [
     [
